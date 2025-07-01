8251b09eb44af04853bc721496419611e464cf99
Chris Brinker <chrisbrinker@pm.me>
2025-07-01 20:04:51 +0200

    rework: using tcgdex api

---
+++ src/data/conversions.ts
@@ -0,0 +1,28 @@
+export const energyNameMap: Record<string, string> = {
+  "Basic {L} Energy": "Lightning Energy",
+  "Basic {D} Energy": "Darkness Energy",
+  "Basic {F} Energy": "Fighting Energy",
+  "Basic {P} Energy": "Psychic Energy",
+  "Basic {M} Energy": "Metal Energy",
+  "Basic {W} Energy": "Water Energy",
+  "Basic {G} Energy": "Grass Energy",
+  "Basic {R} Energy": "Fire Energy",
+};
+
+export const intlToJapaneseSetMap: Record<string, string> = {
+  svp: "svp", // Promos
+  svi: "sv01", // Scarlet & Violet
+  pal: "sv02", // Paldea Evolved
+  obf: "sv03", // Obsidian Flames
+  mew: "sv03.5", // 151
+  par: "sv04", // Paradox Rift
+  paf: "sv04.5", // Paldean Fates
+  tef: "sv05", // Temporal Forces
+  twm: "sv06", // Twilight Masquerade
+  sfa: "sv06.5", // Shrouded Fable
+  scr: "sv07", // Stellar Crown
+  ssp: "sv08", // Surgin Sparks
+  pre: "sv08.5", // Prismatic Evolutions
+  jtg: "sv09", // Journey Together
+  dri: "sv10", // Destined Rivals
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,63 @@
-import PTCGDeckValidator from "./PTCGDeckValidator.js";
-import { readFileSync } from "fs";
-import "dotenv/config";
+import TCGdex, { Query } from "@tcgdex/sdk";
+import PTCGDeckValidator, {
+  type ValidationResult,
+} from "./PTCGDeckValidatorTCGdex.js";
+import { readFileSync, writeFileSync } from "fs";
 
 const validator = new PTCGDeckValidator();
-const testDecklist = readFileSync("debug/decklist.txt").toString();
-await validator.setDeck(testDecklist);
-const validResult = await validator.validateDecklist();
+const testDecklist = readFileSync("debug/gardi.txt").toString();
 
-console.dir(validResult);
+const tdex = new TCGdex("en");
+const res = await tdex.card.list(Query.create().equal("name", "Ultra Ball"));
+
+const balls: unknown[] = [];
+
+const results = await Promise.allSettled(
+  res.map(async (token) => {
+    const card = await token.getCard();
+    balls.push(card);
+  })
+);
+
+function safeStringify(obj: any) {
+  const seen = new WeakSet();
+  return JSON.stringify(
+    obj,
+    function (key, value) {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) return "[Circular]";
+        seen.add(value);
+      }
+      return value;
+    },
+    2
+  ); // pretty print
+}
+const niceBalls = safeStringify(balls);
+writeFileSync("balls.json", niceBalls);
+//const fu = await validator.queryCardString("sv09-155", name);
+
+//console.log(fu);
+//go();
+
+async function go() {
+  const validationResult: ValidationResult = await validator.check(
+    testDecklist
+  );
+  console.log("Validation Result");
+  console.dir(validationResult);
+
+  //const validResult = await validator.validateDecklist();
+
+  //console.dir(validResult);
+}
+/*
+import TCGdex, { Query } from "@tcgdex/sdk";
+
+const tcgdex = new TCGdex("de");
+const card = await tcgdex.card.list(
+  Query.create().contains("name", "Erstklassiger Fänger") // Match exact name
+);
+
+console.dir(await card[0].getCard(), { depth: 10 });
+*/
